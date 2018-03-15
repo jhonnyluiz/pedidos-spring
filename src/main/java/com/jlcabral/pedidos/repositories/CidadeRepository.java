cb6885a914dbbbfc9f4f0f2378ea72a5396ee5da
Jhonny Luiz <jhonny_luiz_c@hotmail.com>
2018-03-15 16:33:07 -0300

    Criação de cidade e estado

---
+++ src/main/java/com/jlcabral/pedidos/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.jlcabral.pedidos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jlcabral.pedidos.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Long>{
+
+}
