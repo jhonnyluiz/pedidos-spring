62bc01fa84d1e38b14fd52578eead991c8ae40d5
Jhonny Luiz <jhonny_luiz_c@hotmail.com>
2018-03-15 15:18:35 -0300

    Criando instanciação na inicialização

--- src/main/java/com/jlcabral/pedidos/PedidosApplication.java
+++ src/main/java/com/jlcabral/pedidos/PedidosApplication.java
@@ -1,12 +1,30 @@
 package com.jlcabral.pedidos;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PedidosApplication {
+import com.jlcabral.pedidos.domain.Categoria;
+import com.jlcabral.pedidos.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class PedidosApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PedidosApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	
 	}
 }
--- src/main/java/com/jlcabral/pedidos/resources/CategoriaResource.java
+++ src/main/java/com/jlcabral/pedidos/resources/CategoriaResource.java
@@ -3,6 +3,7 @@ package com.jlcabral.pedidos.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,19 +17,12 @@ import com.jlcabral.pedidos.services.CategoriaService;
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
+	@Autowired
 	private CategoriaService service;
 	
 	@RequestMapping(value="/{id}", method=RequestMethod.GET)
 	public ResponseEntity<?> listar(@PathVariable Long id) {
 		Categoria obj = service.buscarPorId(id);
-		
 		return ResponseEntity.ok().body(obj);
-		
-//		Categoria cat1 = new Categoria(null, "Informática");
-//		Categoria cat2 = new Categoria(null, "Escritório");
-//		List<Categoria> lista = new ArrayList<>();
-//		lista.add(cat1);
-//		lista.add(cat2);
-//		return lista;
 	}
 }
