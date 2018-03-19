c4e7327a25203bb5608ff9600e39269ab6e7ad1e
Jhonny Luiz <jhonny_luiz_c@hotmail.com>
2018-03-19 16:13:53 -0300

    Classe de exceção personalizada de Categoria

--- src/main/java/com/jlcabral/pedidos/services/CategoriaService.java
+++ src/main/java/com/jlcabral/pedidos/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.jlcabral.pedidos.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.jlcabral.pedidos.domain.Categoria;
 import com.jlcabral.pedidos.repositories.CategoriaRepository;
+import com.jlcabral.pedidos.services.exceptions.DataIntegrityException;
 import com.jlcabral.pedidos.services.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -19,13 +21,23 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjetoNaoEncontradoException(id.toString(), Categoria.class));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
+		findById(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Long id) {
+		findById(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possua produtos");
+		}
 	}
 }
