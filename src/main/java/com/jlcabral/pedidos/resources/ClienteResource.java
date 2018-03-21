9b95f3105dec37ca6e18da9f940d21164b7ed78f
Jhonny Luiz <jhonny_luiz_c@hotmail.com>
2018-03-21 08:50:50 -0300

    Put, delete e update para cliente

--- src/main/java/com/jlcabral/pedidos/resources/ClienteResource.java
+++ src/main/java/com/jlcabral/pedidos/resources/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.jlcabral.pedidos.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jlcabral.pedidos.domain.Cliente;
+import com.jlcabral.pedidos.dto.ClienteDTO;
 import com.jlcabral.pedidos.services.ClienteService;
 
 @RestController
@@ -21,5 +30,37 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> findById(@PathVariable Long id) {
 		Cliente obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDTO, @PathVariable Long id) {
+		objDTO.setId(id);
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = service.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@RequestMapping(value="/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value="page", defaultValue="0") Integer page, 
+			@RequestParam(value="linesPerPage", defaultValue="24") Integer linesPerPage, 
+			@RequestParam(value="orderBy", defaultValue="nome") String orderBy, 
+			@RequestParam(value="direction", defaultValue="ASC") String direction) {
+		Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDTO = list.map(obj -> new ClienteDTO(obj));
+		return ResponseEntity.ok().body(listDTO);
 	}
 }
--- src/main/java/com/jlcabral/pedidos/services/ClienteService.java
+++ src/main/java/com/jlcabral/pedidos/services/ClienteService.java
@@ -1,21 +1,60 @@
 package com.jlcabral.pedidos.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.jlcabral.pedidos.domain.Cliente;
+import com.jlcabral.pedidos.dto.ClienteDTO;
 import com.jlcabral.pedidos.repositories.ClienteRepository;
+import com.jlcabral.pedidos.services.exceptions.DataIntegrityException;
 
 @Service
 public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
-	
+
 	public Cliente findById(Long id) {
 		Optional<Cliente> obj = repo.findById(id);
 		return obj.orElse(null);
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Long id) {
+		findById(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir um cliente que possua pedidos");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		return repo.findAll(PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy));
+	}
+
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 }
