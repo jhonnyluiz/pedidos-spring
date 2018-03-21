1ad2fe4a4683d047427a791951fde67dc7fc0298
Jhonny Luiz <jhonny_luiz_c@hotmail.com>
2018-03-21 08:58:36 -0300

    Correções em categoria service e construtor de Pagamento

--- src/main/java/com/jlcabral/pedidos/services/CategoriaService.java
+++ src/main/java/com/jlcabral/pedidos/services/CategoriaService.java
@@ -33,8 +33,9 @@ public class CategoriaService {
 	}
 
 	public Categoria update(Categoria obj) {
-		findById(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
 
 	public void delete(Long id) {
@@ -56,5 +57,9 @@ public class CategoriaService {
 
 	public Categoria fromDTO(CategoriaDTO objDTO) {
 		return new Categoria(objDTO.getId(), objDTO.getNome());
+	}
+
+	private void updateData(Categoria newObj, Categoria obj) {
+		newObj.setNome(obj.getNome());
 	}
 }
