68e0a2665836ff5cb65ff90d223ba1dee7ad74bd
Jhonny Luiz <jhonny_luiz_c@hotmail.com>
2018-03-22 19:03:17 -0300

    Banco Mysql e strategy create

--- src/main/java/com/jlcabral/pedidos/config/DevConfig.java
+++ src/main/java/com/jlcabral/pedidos/config/DevConfig.java
@@ -3,7 +3,6 @@ package com.jlcabral.pedidos.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,23 +14,15 @@ import com.jlcabral.pedidos.services.DBService;
  * @date 22 de mar de 2018
  */
 @Configuration
-@Profile("test")
-public class TestConfig {
-
-	private static final String STRATEGY_CREATE = "create";
+@Profile("dev")
+public class DevConfig {
 
 	@Autowired
 	private DBService dbService;
-	
-	@Value("${spring.jpa.hibernate.ddl-auto}")
-	private String strategy;
-	
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
-		if(strategy.equals(STRATEGY_CREATE)) {
-			dbService.instantiateTestDatabase();
-			return true;
-		}
-		return false;
+		dbService.instantiateTestDatabase();
+		return true;
 	}
 }
