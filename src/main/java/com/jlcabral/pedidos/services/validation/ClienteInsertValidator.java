212e4e1c02177a8d7bf283f1952866f9731c3f08
Jhonny Luiz <jhonny_luiz_c@hotmail.com>
2018-03-22 09:04:23 -0300

    Validação email existente pelo DTO

--- src/main/java/com/jlcabral/pedidos/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/jlcabral/pedidos/services/validation/ClienteInsertValidator.java
@@ -6,8 +6,12 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.jlcabral.pedidos.domain.Cliente;
 import com.jlcabral.pedidos.dto.ClienteNewDTO;
 import com.jlcabral.pedidos.enuns.TipoCliente;
+import com.jlcabral.pedidos.repositories.ClienteRepository;
 import com.jlcabral.pedidos.resources.exception.FieldMessage;
 import com.jlcabral.pedidos.services.validation.utils.BR;
 
@@ -17,6 +21,9 @@ import com.jlcabral.pedidos.services.validation.utils.BR;
  */
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -31,6 +38,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		
+		if(aux != null) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
 		}
 		
 		for (FieldMessage e : list) {
